e0825813e4e24fd45811e296721b4f8241c77003
songhee1 <ghenrhkwk88@naver.com>
2023-11-04 01:07:05 +0900

    [feat][week02]03-P42747

--- songhee/week02/P42747.java
+++ songhee/week02/P42747.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
         int arr[] = new int[10001];
@@ -7,18 +6,25 @@ class Solution {
             arr[a]++;
         }
 
+        /*
+        [6,5,4,1,1,1]
+   arr[]:0 1 2 3 4 5 6.... 10000
+           3     1 1 1
+     sum:  4     3 2 1
+
+        */
+         */
+         */
+
         int sum = 0;
         int minus = 0;
 
         for(int j=10000;j>=0;j--){
-
             sum += arr[j];
             if(sum >= j){
                 return j;
             }
         }
-
         return 0;
-
     }
 }
