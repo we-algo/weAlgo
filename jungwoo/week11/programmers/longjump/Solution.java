facd3b34d6b89bda01632c0d1b93915f9c34d32f
LEEJUNGWOO <jungwoo1002@gmail.com>
2024-01-03 00:37:03 +0900

    멀리뛰기 / 10분

---
+++ jungwoo/week11/programmers/longjump/Solution.java
@@ -0,0 +1,15 @@
+package algorithmstudy.programmers.longjump;
+
+class Solution {
+    public long solution(int n) {
+        int a = 1, b = 1, temp;
+
+        for (int i = 2; i <= n; i++) {
+            temp = (a + b) % (1234567);
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+}
